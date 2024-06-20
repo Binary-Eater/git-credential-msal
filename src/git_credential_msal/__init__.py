3730c6bff6722fe036bcfa8a9f5a120d117b75e3
Rahul Rameshbabu <sergeantsagara@protonmail.com>
2024-06-19 19:39:25 -0700

    Support python standard metadata for packaging git-credential-msal
    Will help simplify distribution-specific packaging work.
    
    Link: https://peps.python.org/pep-0517/
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

---
+++ src/git_credential_msal/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("git_credential_msal")
+except PackageNotFoundError:
+    # package is not installed
+    pass
